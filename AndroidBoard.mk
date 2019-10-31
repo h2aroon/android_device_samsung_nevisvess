11f59f4bdcfec1726c15ebf37fd23661a2029233
Haroon Abu nazir <46633410+h2aroon@users.noreply.github.com>
2019-10-31 13:14:50 +0300

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/samsung/nevisp/AndroidBoardVendor.mk
+-include vendor/samsung/nevisvess/AndroidBoardVendor.mk
